f1af4599145f7e024431d9d15ce806a0ad994128
Hunter Black <huntergblack@gmail.com>
2018-10-28 18:15:58 -0500

    added youtube lyte template

---
+++ scripts/youtubeLyte.js
@@ -0,0 +1,71 @@
+// Set YouTube API options
+const CLIENT_ID = '1039077288974-8e027o4g9h12f9g5emnspuvrtuf08br0.apps.googleusercontent.com';
+// Array of API discovery doc URLs for APIs used by the quickstart
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+
+// Authorization scopes required by the API. If using multiple scopes,
+// separated them with spaces.
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
+
+const defaultChannel = 'techguyweb';
+
+// Load Auth2 library
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+// Init API client library and set up signin listeners
+function initClient() {
+    gapi.client.init({
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+    }).then(() => {
+        // Listen for signin state changes
+        gapi.auth2.getAuthInstance().isSignedIng.listen(updateSigninStatus);
+        // Handle initial signin state
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        // Handle clicks of login and logout button
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+// Update UI signin state changes
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        content.style.display = 'block';
+        videoContainer.style.display = 'block';
+        getChannel(defaultChannel);
+    }
+    else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+        content.style.display = 'none';
+        videoContainer.style.display = 'none';   
+    }
+}
+
+// Handle login
+function handleAuthClick() {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+// Handle logout
+function handleSignoutClick() {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+// Get channel from API
+function getChannel(channel) {
+    console.log(channel);
+}
