76f5f94cf1e20116284f07ca56a90a0fe2482cbe
Hunter Black <huntergblack@gmail.com>
2018-10-30 12:03:59 -0500

    changed default channel for testing

--- scripts/youtubeLyte.js
+++ scripts/youtubeLyte.js
@@ -14,7 +14,7 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'thetechguy';
+const defaultChannel = 'TetraNinja';
 
 // Load Auth2 library
 function handleClientLoad() {
